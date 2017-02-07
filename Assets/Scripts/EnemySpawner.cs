403cc1e74cc530607893666a6128858465067dc6
szimano <szimano@szimano.org>
2017-02-07 08:35:10 +0100

    initial commit with some game stuff

---
+++ Assets/Scripts/EnemySpawner.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour {
+
+	public GameObject enemyPrefab;
+
+	// Use this for initialization
+	void Start () {
+
+		foreach(Transform child in transform) {
+			GameObject enemy = Instantiate(enemyPrefab, child.transform.position, Quaternion.identity);
+			enemy.transform.parent = child;	
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+	public float speed;
+	public float padding = 1f;
+
+	float xmin;
+	float xmax;
+
+	void Start () {
+		float distance = transform.position.z - Camera.main.transform.position.z;
+		Vector3 leftMost = Camera.main.ViewportToWorldPoint(new Vector3(0, 0, distance));
+		Vector3 rightMost = Camera.main.ViewportToWorldPoint(new Vector3(1, 0, distance));
+
+		xmin = leftMost.x + padding;
+		xmax = rightMost.x - padding; 
+	}
+	
+	void Update () {
+		if (Input.GetKey("right")) {
+			gameObject.transform.position += Vector3.right * speed * Time.deltaTime;
+		} else if (Input.GetKey("left")) {
+			gameObject.transform.position += Vector3.left * speed * Time.deltaTime;
+		} 
+
+		// stay within gamespace
+		float newX = Mathf.Clamp(transform.position.x, xmin, xmax);
+		transform.position = new Vector3(newX, transform.position.y, transform.position.z);
+	}
+}
